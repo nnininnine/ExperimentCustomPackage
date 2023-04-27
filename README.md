2f527a24f4f3ed1fa76e681c356ce7a00de9f10f
Nuttapon Buaban <Nuttapon.Buaban@capco.com>
2023-04-27 11:38:16 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ExperimentCustomPackage
+
+A description of this package.
---
+++ Tests/ExperimentCustomPackageTests/ExperimentCustomPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ExperimentCustomPackage
+
+final class ExperimentCustomPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ExperimentCustomPackage().text, "Hello, World!")
+    }
+}
